4f8bdad8ee66809dc6d17d3efb258bd8466714f8
Phillip Lam <plam89@ucla.edu>
2020-11-05 16:31:06 -0800

    fixed interval issues and absolute, fixed css issues

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var carData = { direction: null };
+var carData = { direction: null, location: { x: 0, y: 0 } };
 var $car = document.querySelector('img');
 
 document.addEventListener('keydown', function (event) {
+
   if (event.key === 'ArrowUp') {
     carData.direction = 'north';
     $car.className = 'north';
@@ -18,5 +19,13 @@ document.addEventListener('keydown', function (event) {
     carData.direction = 'west';
     $car.className = 'west';
   }
-
+  if (event.key === ' ') {
+    var interval = setInterval(function () {
+      carData.location.x += 4;
+      $car.style.left = carData.location.x + 'px';
+      if (carData.location.x > 1500) { // stops when off screen
+        clearInterval(interval);
+      }
+    }, 16);
+  }
 });
